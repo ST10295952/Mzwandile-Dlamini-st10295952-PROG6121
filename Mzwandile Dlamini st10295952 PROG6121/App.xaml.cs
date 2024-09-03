f17faf1874e0c0827591a5c4ec8a8245787c8b24
Mzwandile Ayabonga Dlamini <ST10295952@rcconnect.edu.za>
2024-09-03 07:07:54 +0000

    Add project files.

---
+++ Mzwandile Dlamini st10295952 PROG6121/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Mzwandile_Dlamini_st10295952_PROG6121
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
